d89a4c7cb9e761644034cefc31ec554ecac6c948
DanielKork <101044386+DanielKork@users.noreply.github.com>
2024-04-01 17:13:01 +0300

    1

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Import routers
-const caloriesRouter = require('./routes/addcalories');//12
+const caloriesRouter = require('./routes/addcalories');
 const reportRouter = require('./routes/report');
 const aboutRouter = require('./routes/about');
 
